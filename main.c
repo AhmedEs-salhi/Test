0206b1ebf1f7126a77055306bade8df78b7e5a09
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-03-21 23:09:58 +0000

    my commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main()
+{
+	int addition;
+	int substruction;
+
+	addition = add(6, 5);
+	substruction = sub(9, 4);
+
+	printf("Addition = %d\n", addition);
+	printf("Substruction = %d\n", substruction);
+
+	return 0;
+}
